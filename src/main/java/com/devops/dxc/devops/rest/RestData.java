226590406016abab0a4f64fe22e09560815e07cc
Andres Aldana <batroid@gmail.com>
2021-02-06 22:58:56 -0300

    Se agrega configuración de cors y pagina html con formulario de calculo 10%.

--- src/main/java/com/devops/dxc/devops/rest/RestData.java
+++ src/main/java/com/devops/dxc/devops/rest/RestData.java
@@ -5,12 +5,9 @@ import java.util.logging.Logger;
 import com.devops.dxc.devops.model.Dxc;
 import com.devops.dxc.devops.service.DxcService;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping(path = "/rest/msdxc")
 public class RestData {
